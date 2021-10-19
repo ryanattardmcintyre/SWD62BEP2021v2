dabc01d66b3c67af8e7ebab1e76b4298698944e1
Ryan Attard <ryanattard@gmail.com>
2021-10-19 15:54:05 +0200

    Initial Commit

---
+++ DataAccess/Context/BloggingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Context
+{
+    public class BloggingContext: DbContext
+    {
+
+
+
+    }
+}
---
+++ Domain/Models/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Blog
+    {
+        [Key] //sets this as primary key
+              //sets this as an identity
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string LogoImageUrl { get; set; }
+
+    }
+}
